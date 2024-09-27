2d42ae9d34d5af04bd777b6c7fed022697b04ce4
vKail <ajurado5411@uta.edu.ec>
2024-09-27 12:19:58 -0500

    project created

---
+++ src/main/java/com/adrian/champlonshipfootball/ChamplonshipFootballApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.champlonshipfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamplonshipFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamplonshipFootballApplication.class, args);
+	}
+
+}
